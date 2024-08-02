ba4d25d062aea2b49186daafe29915e6aa31e621
koushik vuriti <koushikvuriti0212@gmail.com>
2024-08-02 22:48:03 +0530

    StudyBuddy

--- base/views.py
+++ base/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.db.models import Q
-from .models import Room,Topic,Message
-from .forms import RoomForm,UserForm
+from .models import Room,Topic,Message,User
+from .forms import RoomForm,UserForm,MyUserCreationForm
 
 # Create your views here.
 
@@ -17,11 +15,11 @@ def loginPage(request):
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username').lower()
+        username = request.POST.get('email').lower()
         password = request.POST.get('password').lower()
 
         try:
-            user = User.objects.get(username=username)
+            user = User.objects.get(email=email)
         except:
             messages.error(request,'User does not exist')
 
@@ -43,9 +41,9 @@ def logoutUser(request):
 
 def registerPage(request):
     page = 'register'
-    form = UserCreationForm()
+    form = MyUserCreationForm()
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = MyUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
@@ -172,7 +170,7 @@ def updateUser(request):
     form = UserForm(instance=user)
 
     if request.method == "POST":
-        form = UserForm(request.POST,instance=user)
+        form = UserForm(request.POST,request.FILES,instance=user)
         if form.is_valid():
             form.save()
             return redirect('user-profile',pk=user.id)
